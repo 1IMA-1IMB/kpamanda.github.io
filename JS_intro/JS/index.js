6e85587388ce42993c5d7c3be89a3895e2ad700b
Amanda <mandarox@icloud.com>
2024-01-03 13:49:53 +0100

    js oppgave

--- JS_intro/JS/index.js
+++ JS_intro/JS/index.js
@@ -1,3 +1,25 @@
 // seperation of concernsv - å separere javascript fra html
 // string er en datatype. heltall. en annen datatype er desimaltall og sånne ting. en string er tekst med '' rundt teksten. tekst er ikke en string, men en string kan være tekst. å gjøre noe til en string er å lagge anførselstegn rundt.
-console.log('hello world')
+
+// a)
+let x = "Amanda";
+let y = "16";
+let z = "Hei " + x + ", du er " + y + "år gammel";
+console.log(z)
+
+// b)
+// kommandoen for å lage en variabel heter "let". det er en lignende kommando som heter "var" og de er veldig like, men vi skal bruke "let".
+
+let erenelev = true;
+
+if( !erenelev ) {
+
+    console.log( "du er ikke en elev!" );
+
+} else {
+
+    console.log( "du er en elev!" );
+
+}
+
+// på console her fikk jeg "du er en elev!" fordi
