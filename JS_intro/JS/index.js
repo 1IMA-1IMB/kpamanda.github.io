ec4c00837bdb9d872a90cb2a46b8c81783f458fe
Amanda <mandarox@icloud.com>
2024-01-03 10:36:47 +0100

    js intro

---
+++ JS_intro/JS/index.js
@@ -0,0 +1,3 @@
+// seperation of concernsv - å separere javascript fra html
+// string er en datatype. heltall. en annen datatype er desimaltall og sånne ting. en string er tekst med '' rundt teksten. tekst er ikke en string, men en string kan være tekst. å gjøre noe til en string er å lagge anførselstegn rundt.
+console.log('hello world')
