082236c9b392c960d0b375521b6926d2ef1ffdb8
Amanda <mandarox@icloud.com>
2024-01-08 15:57:55 +0100

    javaaaaa

--- JS_intro/JS/index.js
+++ JS_intro/JS/index.js
@@ -1,3 +1,4 @@
+
 // seperation of concernsv - å separere javascript fra html
 // string er en datatype. heltall. en annen datatype er desimaltall og sånne ting. en string er tekst med '' rundt teksten. tekst er ikke en string, men en string kan være tekst. å gjøre noe til en string er å lagge anførselstegn rundt.
 
@@ -5,21 +6,50 @@
 let x = "Amanda";
 let y = "16";
 let z = "Hei " + x + ", du er " + y + "år gammel";
-console.log(z)
+console.log(z);
 
 // b)
 // kommandoen for å lage en variabel heter "let". det er en lignende kommando som heter "var" og de er veldig like, men vi skal bruke "let".
 
+
+//c)
 let erenelev = true;
 
+//d)
 if( !erenelev ) {
-
-    console.log( "du er ikke en elev!" );
-
-} else {
 
     console.log( "du er en elev!" );
 
+} else {
+
+    console.log( "du er ikke en elev!" );
+
 }
 
-// på console her fikk jeg "du er en elev!" fordi
+// på console her fikk jeg "du er en elev!" fordi 
+
+//del 2 a)
+let svar = 2 + 2;
+console.log(svar);
+
+//b)
+let svar1 = 11 * 1;
+let svar2 = 11 * 2;
+let svar3 = 11 * 3;
+let svar4 = 11 * 4;
+let svar5 = 11 * 5;
+let svar6 = 11 * 6;
+let svar7 = 11 * 7;
+let svar8 = 11 * 8;
+let svar9 = 11 * 9;
+let svar10 = 11 * 10;
+let svar11 = 11 * 11;
+console.log(svar1, svar2, svar3, svar4, svar5, svar6, svar7, svar8, svar9, svar10, svar11);
+
+//c)
+
+function test(number1, number2) {
+    console.log(number1, number2)
+}
+
+test(3, 7);
