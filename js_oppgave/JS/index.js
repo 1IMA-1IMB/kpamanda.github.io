54d6c36026ebf66eba64247bbf332d2b6b13ddec
Amanda <mandarox@icloud.com>
2024-02-10 23:35:17 +0100

    æææææææææææ

--- js_oppgave/JS/index.js
+++ js_oppgave/JS/index.js
@@ -1,40 +1,67 @@
+function konvertering() {
+    const gallon = 3.78541178;
+    const ounces = 0.035274;
+    const cups = 0.236588;
+    const spiseskjeer = 0.0147868;
+    const liter = 1;
 
-  
-function velgMålenhet() {
+    const input = parseFloat(document.getElementById("input").value);
+    const from = document.getElementById("from").value;
+    const to = document.getElementById("to").value;
+    let output;
 
-const gallons = 3.78541;
-const ounces = 0.0295735;
-const cups = 0.236288;
-const spiseskjeer = 0.0147868;
-const input = document.getElementById("input").value;
-const measure = document.getElementById("measure");
-
-    if(measure.value == "spiseskjeer"){
-
-        document.getElementById("output").innerHTML = ("det er " + input * spiseskjeer + " liter i " + input + " spiseskje");
-    
-    } else if (measure.value == "ounces"){
-    
-        document.getElementById("output").innerHTML = ("det er " + input * ounces + " liter i " + input + " ounces");
-    
-    } else if (measure.value == "cups"){
-    
-        document.getElementById("output").innerHTML = ("det er " + input * cups + " liter i " + input + " cups");
-    
-    } else if (measure.value == "gallons"){
-    
-        document.getElementById("output").innerHTML = ("det er " + input * gallons + " liter i " + input + " gallons");
-    
+    switch (from) {
+        case "gallon":
+            output = input * gallon;
+            break;
+        case "ounces":
+            output = input * ounces;
+            break;
+        case "cups":
+            output = input * cups;
+            break;
+        case "spiseskjeer":
+            output = input * spiseskjeer;
+            break;
+        case "liter":
+            output = input * liter;
+            break;
+        default:
+            output = input;
     }
 
+    switch (to) {
+        case "gallon":
+            output /= gallon;
+            break;
+        case "ounces":
+            output /= ounces;
+            break;
+        case "cups":
+            output /= cups;
+            break;
+        case "spiseskjeer":
+            output /= spiseskjeer;
+            break;
+        case "liter":
+            output /= liter;
+            break;
+        default:
+            output = input;
+    }
+
+    document.getElementById("output").innerText = "Konvertert verdi: " + output.toFixed(2);
 }
 
-
-
-
-
-
-
+const panildrom = input;
+const endret = palindrom.split("").reverse().join("")
+if (palindrom == endret){
+    console.log("palindrom")
+    //skal endres på en p tag hvis det er riktig
+} else {
+    console.log("ikke palindrom")
+    //skal endres på en p tag hvis det er feil
+}
 
 
 
---
+++ js_opplæring/js/index.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+function test() {
+    let test = 0;
+}
+
+
+function clickMe() {
+    document.getElementById("test-output").innerHTML = "hello world";
+    let test = 1;
+    console.log("test");
+
+    return(test);
+}
+
+console.log(test);
+
+// for (let i = 0; i < 1000; i++){
+//     console.log(i);
+// }
+
+// const arr = [1,2,3,4]
+// for (let i = 0; i < arr.length; 1++) {
+//     console.log(arr[i]);
+// }
